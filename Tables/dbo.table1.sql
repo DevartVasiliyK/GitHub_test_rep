00e6c1ed0a4a0d76d846d9e4e37e408e63ab7478
test <test@devart>
2017-05-18 12:49:14 +0300

    Fix #Test_issue

---
+++ Tables/dbo.table1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[table1] (
+  [column1] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
+
+CREATE CLUSTERED INDEX [IDX_table1_column1]
+  ON [dbo].[table1] ([column1])
+  ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'tre', 'SCHEMA', N'dbo', 'TABLE', N'table1'
+GO
